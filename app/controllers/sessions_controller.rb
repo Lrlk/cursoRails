45dec9d0da9d1fba9894b52521ab44846f03fbdd
Lucas Lopes <Lucasrlopes2002@gmail.com>
2021-11-23 15:22:57 -0300

    User autentication, but the dropdown menu dosent work

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    user = User.find_by(email: params[:session] [:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sing_in(user)
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = 'Email ou senha invalidos'
+      render 'new'
+    end
+  end
+
+  def destroy
+    sing_out
+    flash[:success] = 'Logout com sucesso'
+    redirect_to entrar_path
+  end
+
+end
